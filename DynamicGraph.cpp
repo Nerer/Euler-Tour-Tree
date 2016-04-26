fca562056c4792fdf74695002f55fda628f4aa6c
Susan Long <dlsubjam@gmail.com>
2016-04-26 15:15:43 +0800

    link added

--- DynamicGraph.cpp
+++ DynamicGraph.cpp
@@ -5,7 +5,112 @@
 #include <vector>
 using namespace std;
 
-class SplayTree{};
+class Treap
+{
+public:
+    Node *join(Node *l, Node *r)
+    {
+        if(!l) return r;
+        if(!r) return l;
+        Node *t = NULL;
+        
+        int h;
+        long long num = 0;
+        for(h = 0; ; ++h)
+        {
+            if(h >= sizeof(num) * 8 - 2)//QAQAQ
+            {
+                t = join(l->right, r->left);
+                num = num << 2 | 1;
+                ++h;
+                break;
+            }
+            num <<= 1;
+            if(l->priority < r->prioirity)
+            {
+                Node *tmp = l->right;
+                if(!tmp)
+                {
+                    t = r;
+                    r = r->parent;
+                    break;
+                }
+                l = tmp;
+            }
+            else
+            {
+                Node *tmp = r->left;
+                if(!tmp)
+                {
+                    t = l;
+                    l = l->parent;
+                    break;
+                }
+                r = tmp;
+                num |= 1;
+            }
+        }
+        for(; h >= 0; --h)
+        {
+            if(!(num & 1))
+            {
+                Node *p = l->parent;
+                t = l->linkr(t);
+                l = p;
+            }
+            else
+            {
+                Node *p = r->parent;
+                t = r->linkl(t);
+                r = p;
+            }
+            num >>= 1;
+        }
+        
+        return t;
+    }
+    //cut the line between t->left and t
+    pair<Node*, Node*> split(Node* t)
+    {
+        Node *p;
+        Node *l = t->left;
+        Node *r = t;
+        Node::cut(l);
+        t->linkl(NULL);
+        while(t->parent)
+        {
+            p = t->parent;
+            t->parent = NULL;
+            if(p->left == t) r = p->linkl(r);
+            else l = p->linkr(l);
+            t = p;
+        }
+        return pair(l, r);
+    }
+    Node* comb(Node* h, Node* t)
+    {
+        if(!t) return h;
+        Node* tmp = NULL;
+        while(1)
+        {
+            if(h->priority < t->priority)
+            {
+                Node* p = t->parent;
+                tmp = h->linkr(t);
+                t = p;
+                break;
+            }
+            Node* l = t->left;
+            if(!l) {tmp = h; break;}
+            t = l;
+        }
+        while(t)
+        {
+            tmp = t->linkl(tmp); t = t->parent;
+        }
+        return tmp;
+    }
+};
 
 
 class Forest
@@ -48,12 +153,19 @@ private:
         }
         Node *linkl(Node *c)
         {
+            if(c) left = c, c->parent = this;
+            return update();
         }
         Node *linkr(Node *c)
         {
+            if(c) right = c, c->parent = this;
+            return update();
         }
         Node *linklr(Node *l, Node *r)
         {
+            if(l) left = l, l->parent = this;
+            if(r) right = r, r->parent = this;
+            return update();
         }
         Node *cut(Node *t)
         {
@@ -79,8 +191,12 @@ private:
             }
         }
     }
-    Node::SplayTree<Node> bst;
+    Node::Treap<Node> bst;
     vector<Node> nodes;
+    
+    void firstArcChanged(int v, int a, int b)
+    {
+    }
     
 public:
     void init(int N)
@@ -89,12 +205,44 @@ public:
         firstArc.assign(N, -1);
         
         nodes.assign(M * 2, Node());
-        for(int i = 0; i < M * 2; ++i) nodes[i].priority = bst.nextRand();//QAQAQ
+        for(int i = 0; i < M * 2; ++i) nodes[i].priority = rand();
         EMark.assign(M, 0);
         VMark.assign(N, 0);
     }
     void link(int ti, int u, int v)
     {
+        int a1 = arc1(ti), a2 = arc2(ti);
+        if(u > v) swap(a1, a2);
+        int uu = firstArc[u], vv = firstArc[v];
+        
+        Node *l, *m, *r;
+        if(uu != -1)
+        {
+            pair<Node*, Node*> p = bst.split(&node[uu]);
+            m = bst.join(p.second, p.first);
+        }
+        else
+        {
+            m = NULL;
+            firstArc[u] = a1;
+            firstArcChanged(u, -1, a1);
+        }
+        if(vv != -1)
+        {
+            pair<Node*, Node*> p = bst.split(&node[vv]);
+            l = p.first, r = p.second;
+        }
+        else
+        {
+            l = r = NULL;
+            firstArc[v] = a2;
+            firstArcChanged(v, -1, a2);
+        }
+        
+        m = bst.comb(&nodes[a2], m);
+        r = bst.comb(&nodes[a1], r);
+        
+        bst.join(bst.join(l, m), r);
     }
     bool connected(int u, int v)
     {
@@ -224,6 +372,8 @@ public:
 
 int main()
 {
+    srand(time(0));
+    
     int N, M;
     scanf("%d%d", &N, &M);
     
